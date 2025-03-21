9cddeda5c7ba8881d225f13a68c30f93b2cb3e63
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-21 23:40:21 +0530

    add IndexScreen component with initial state for stocks

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import { Text } from "react-native-reanimated/lib/typescript/Animated";
+export  default function IndexScreen (){
+    const [stocks,setStocks]=useState<string, []Candle>({})
+}
