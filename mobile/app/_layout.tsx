1418226be50dd7acab226aac40c7d6c1034cc309
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-21 23:57:13 +0530

    feat: add navigation to stock screen and implement refreshing state in IndexScreen

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,14 @@
 import { Candle } from "@/types/types";
 import { useState } from "react";
+import { router } from "expo-router";
 import { Text } from "react-native-reanimated/lib/typescript/Animated";
 export  default function IndexScreen (){
     const [stocks,setStocks]=useState<Record<string, Candle[]>>({})
+    const [refreshing ,setRefreshing]=useState(false)
+
+    function onGoToStaock(symbol:string){
+        // navigate to stock screen
+        router.push(`/stock/${symbol}`);
+    }
 
 }
