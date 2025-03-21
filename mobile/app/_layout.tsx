52f988477df1667f1847c1c7227dae0a51bb7264
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-21 23:59:57 +0530

    feat: implement stock fetching and rendering in IndexScreen with refresh functionality

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,14 +1,65 @@
+// import { StockRow } from "@/components/StockRow";
+// import { baseUrl } from "@/network";
 import { Candle } from "@/types/types";
-import { useState } from "react";
-import { router } from "expo-router";
-import { Text } from "react-native-reanimated/lib/typescript/Animated";
-export  default function IndexScreen (){
-    const [stocks,setStocks]=useState<Record<string, Candle[]>>({})
-    const [refreshing ,setRefreshing]=useState(false)
+import { router, useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { Alert, FlatList, StyleSheet, View } from "react-native";
 
-    function onGoToStaock(symbol:string){
-        // navigate to stock screen
-        router.push(`/stock/${symbol}`);
+export default function IndexScreen() {
+  const [stocks, setStocks] = useState<Record<string, Candle[]>>({});
+  const [refreshing, setRefreshing] = useState(false);
+
+  function onGoToStock(symbol: string) {
+    router.push(`/stock/${symbol}`);
+  }
+
+  async function fetchStocks() {
+    try {
+      setRefreshing(true);
+      const response = await fetch(`${
+        //baseUrl("http")}/stocks-history`);
+      const json = await response.json();
+      setStocks(json);
+    } catch (error) {
+      if (error instanceof Error) Alert.alert("Error", error.message);
+    } finally {
+      setRefreshing(false);
     }
+  }
 
+  useFocusEffect(useCallback(() => { fetchStocks(); }, []));
+
+  function renderItem({ item: symbol }: { item: string; }) {
+    return (
+    //   <StockRow
+    //     onPress={() => onGoToStock(symbol)}
+    //     symbol={symbol}
+    //     candles={stocks[symbol]}
+    //   />
+    );
+  }
+
+  return (
+    <FlatList
+      data={Object.keys(stocks)}
+      refreshing={refreshing}
+      onRefresh={fetchStocks}
+      keyExtractor={(symbol) => symbol}
+      ItemSeparatorComponent={ItemSeparatorComponent}
+      style={styles.flatList}
+      renderItem={renderItem}
+    />
+  );
 }
+
+const ItemSeparatorComponent = () => <View style={styles.ItemSeparatorComponent} />;
+
+const styles = StyleSheet.create({
+  flatList: {
+    marginBottom: 30
+  },
+  ItemSeparatorComponent: {
+    height: 1,
+    backgroundColor: "lightgray"
+  }
+});
