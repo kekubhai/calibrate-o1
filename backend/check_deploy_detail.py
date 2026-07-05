26bbf150196cac5595595fd613bf5bee7b90cd1b
kekubhai <anirbanghosh060@gmail.com>
2026-07-05 19:58:02 +0530

    cleanup: remove temp scripts, delete SG service, update render.yaml

--- backend/check_deploy_detail.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, json
-
-req = urllib.request.Request(
-    'https://api.render.com/v1/services/srv-d90mcmcm0tmc73duslk0/deploys/dep-d9567ed8nd3s73d5bam0',
-    headers={'Authorization': 'Bearer rnd_WLLP2uY5lagWgF2VtaarNE4biqwM'}
-)
-resp = urllib.request.urlopen(req)
-print(json.dumps(json.loads(resp.read()), indent=2))
